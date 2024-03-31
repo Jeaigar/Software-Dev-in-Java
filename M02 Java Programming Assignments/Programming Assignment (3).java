a7c18edfa1da9e00e1cda39e2e359b57a0a56b39
Jeaigar <86207073+Jeaigar@users.noreply.github.com>
2024-03-31 10:10:51 -0500

    Add files via upload

---
+++ M02 Java Programming Assignments/Programming Assignment (3).java
@@ -0,0 +1,31 @@
+// Define the custom exception
+public class BinaryFormatException extends Exception {
+    public BinaryFormatException(String message) {
+        super(message);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            System.out.println(bin2Dec("1100101"));
+            System.out.println(bin2Dec("1102101")); // This should throw an exception
+        } catch (BinaryFormatException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int bin2Dec(String binaryString) throws BinaryFormatException {
+        if (!binaryString.matches("[01]+")) {
+            throw new BinaryFormatException("Input string: " + binaryString + " is not a binary string.");
+        }
+
+        int decimal = 0;
+        for (int i = 0, len = binaryString.length(); i < len; i++) {
+            if (binaryString.charAt(i) == '1') {
+                decimal += Math.pow(2, len - 1 - i);
+            }
+        }
+        return decimal;
+    }
+}
